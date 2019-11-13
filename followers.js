ee515aadbbaf9a9641ea52b2a59579df66089d56
dannasetia <dannasetia@gmail.com>
2019-11-13 17:49:26 +0700

    first commit

---
+++ followers.js
@@ -0,0 +1,29 @@
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById("authors");
+const url = "https://api.github.com/users/dannasetia11/following";
+fetch(url)
+  .then(resp => resp.json())
+  .then(function(data) {
+    let authors = data;
+    console.log(authors);
+    return authors.map(function(author) {
+      let li = createNode("li"),
+        img = createNode("img"),
+        span = createNode("p");
+      img.src = author.avatar_url;
+      span.innerHTML = `${author.login}`;
+      append(li, img);
+      append(li, span);
+      append(ul, li);
+    });
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
